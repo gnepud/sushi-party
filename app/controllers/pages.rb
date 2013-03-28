24ed17f332a1a4b0dedef075ee76b5e021bec20b
Peng Du <gnepud@gmail.com>
2013-03-29 00:22:08 +0100

    no cache for menu

--- app/controllers/pages.rb
+++ app/controllers/pages.rb
@@ -3,17 +3,17 @@ SushiParty.controllers :pages do
     render 'pages/index'
   end
 
-  get :menu_midi, :map => '/menu-midi', :cache => true do
+  get :menu_midi, :map => '/menu-midi' do
     @menus = MenuType.where(:name => 'Menu midi').first.menus.paginate(page: params[:page], per_page: 12)
     render 'pages/menu_midi'
   end
 
-  get :menu_soir, :map => '/menu-soir', :cache => true do
+  get :menu_soir, :map => '/menu-soir' do
     @menus = MenuType.where(:name => 'Menu soir').first.menus.paginate(page: params[:page], per_page: 12)
     render 'pages/menu_soir'
   end
 
-  get :plateaux, :map => '/plateaux', :cache => true do
+  get :plateaux, :map => '/plateaux' do
     render 'pages/plateaux'
   end
 
